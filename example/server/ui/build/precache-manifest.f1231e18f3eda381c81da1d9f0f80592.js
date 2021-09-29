4035eac0abbbc867eb76785daba5a2f71b1a4025
Muralidharan C <murali.c@ensurity.com>
2021-09-29 11:46:34 +0530

    Added UI Build

---
+++ example/server/ui/build/precache-manifest.f1231e18f3eda381c81da1d9f0f80592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44a628a7cec5d1ecbd33c198be58077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e52bfe3408f8a34239",
+    "url": "/static/css/main.d0b39005.chunk.css"
+  },
+  {
+    "revision": "7d44e5bf4678ce3706bf",
+    "url": "/static/js/2.df210e25.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.df210e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e52bfe3408f8a34239",
+    "url": "/static/js/main.2097578e.chunk.js"
+  },
+  {
+    "revision": "a18433267d9e13d7dacf",
+    "url": "/static/js/runtime-main.cb801cac.js"
+  }
+]);
